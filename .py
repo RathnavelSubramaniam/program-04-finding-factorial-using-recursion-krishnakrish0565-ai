5dc614fa0f08ea010014d0fc45b1806befa46884
krishnakrish0565-ai <krishnakrish0565@gmail.com>
2026-01-12 07:05:27 +0000

    program 4 completed

--- .py
+++ .py
@@ -1,11 +1,11 @@
 def factorial(n):
-    if n == 0 or n ==1:
+    if n==0 or n==1:
         return 1
     else:
          return n*factorial(n-1)
 number=int(input("enter a number:"))
 if number<0:
-      print("\nError: Factorial is not defied for negative numbers.")
+      print("/nerror : Factorial is not defied for negative numbers.")
 else:
      result=factorial(number)
      print(f"\nThe factorial of {number} is{result}")          
